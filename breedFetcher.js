b9d7ddf2ae64f31be34e750eb3c1c679313901ca
TJ <michael.btj@hotmail.com>
2021-12-02 18:44:49 +0000

    lint breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,17 +3,15 @@ const args = process.argv.slice(2);
 const query = args[0];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, function (error, response, body) {
-  // console.error('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-
-  // console.log(typeof body);
-
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-
-  console.log(data[0].description);
-
+request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, function(error, response, body) {
+  if (error) {
+    console.error('error: ',error);
+  } else {
+    const data = JSON.parse(body);
+    if (data) {
+      console.log(data[0].description);
+    } else {
+      console.log("Breed not found");
+    }
+  }
 });
